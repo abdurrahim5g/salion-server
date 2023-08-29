d38aa8b18a042eceb062b4baec9b8ab9993e59f1
f-rahim <freelancerrahim26@gmail.com>
2023-08-29 15:54:36 +0600

    Added Service POST API & clear the code

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Salion server is running"));
+/**
+ *
+ * Setup Middleware
+ */
+app.use(cors());
+app.use(express.json());
 
 /**
- * Require all router from exptenal file
+ * Connect with MongoDB
+ */
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.undypbz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+module.exports = { client };
+
+/**
+ * Require all router from external file
  */
 const useServices = require("./router/services");
 
 /**
  *
- * Create API from exprenal file
+ * Create API from external file
  */
 app.use("/services", useServices);
+
+/**
+ *
+ * Primary API for testing -> server is running
+ */
+app.get("/", (req, res) => res.send("Salion server is running"));
 
 app.listen(port, () =>
   console.log(`Salion server is running http://localhost:${port}`)
--- router/services.js
+++ router/services.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { client } = require("../index");
 
 router.get("/", (req, res) => res.send("All services"));
+
+/**
+ *
+ * Upload service image
+ */
+const run = async () => {
+  try {
+    const serviceCollection = client.db("salion").collection("services");
+
+    // Insert service on database with post API
+    router.post("/", async (req, res) => {
+      const serviceDocs = req.body;
+      const result = await serviceCollection.insertOne(serviceDocs);
+      res.send(result);
+    });
+  } finally {
+    // Write your finally code hear
+  }
+};
+run().catch(console.dir);
 
 module.exports = router;
