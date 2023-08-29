cf88092bc72281742565cd7140859954cc3d591c
f-rahim <freelancerrahim26@gmail.com>
2023-08-29 12:01:15 +0600

    Setup basic services get API

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Salion server is running"));
 
+/**
+ * Require all router from exptenal file
+ */
+const useServices = require("./router/services");
+
+/**
+ *
+ * Create API from exprenal file
+ */
+app.use("/services", useServices);
+
 app.listen(port, () =>
   console.log(`Salion server is running http://localhost:${port}`)
 );
